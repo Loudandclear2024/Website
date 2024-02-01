5a7af3d3a0b85b87b4e10a444a6d0fab8e969990
Preet Sojitra <preet.dev373@gmail.com>
2024-02-01 22:19:37 +0530

    feat: add active link styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,11 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import { Lora } from "next/font/google"
 import { BiAlignRight } from "react-icons/bi"
 import { RxCross2 } from "react-icons/rx"
 import { useState } from "react"
 import localFont from "next/font/local"
-
-const lora = Lora({
-  weight: ["400", "700"],
-  subsets: ["cyrillic", "latin"],
-})
+import { usePathname } from "next/navigation"
 
 const brandBe = localFont({
   src: [
@@ -26,6 +21,10 @@ const brandBe = localFont({
 
 export default function Navbar(props) {
   // console.log(props)
+
+  const pathname = usePathname()
+  // console.log(pathname)
+
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
   return (
     <nav
@@ -35,38 +34,64 @@ export default function Navbar(props) {
     >
       <div className="py-3 px-3 flex items-center justify-between lg:pr-6">
         {/* LOGO DIV */}
-        <div className="flex items-center gap-1">
-          <div>
-            <Image
-              src="/logo.png"
-              height={50}
-              width={50}
-              alt="Loud & Clear"
-              className=" sm:w-20 sm:h-28"
-            />
+        <Link href="/">
+          <div className="flex items-center gap-1">
+            <div>
+              <Image
+                src="/logo.png"
+                height={50}
+                width={50}
+                alt="Loud & Clear"
+                className=" sm:w-20 sm:h-28"
+              />
+            </div>
+            <div>
+              <h1
+                className={` ${brandBe.className}  text-white text-4xl md:text-5xl`}
+              >
+                Loud & Clear
+              </h1>
+            </div>
           </div>
-          <div>
-            <h1
-              className={` ${brandBe.className}  text-white text-4xl md:text-5xl`}
-            >
-              Loud & Clear
-            </h1>
-          </div>
-        </div>
+        </Link>
 
         {/* nav items */}
         <ul className="hidden lg:flex lg:items-center text-white gap-7 text-xl">
           <Link href="/">
-            <li className="nav-link">Home</li>
+            <li
+              className={` cursor-pointer
+              ${pathname === "/" ? "active-nav-link" : "nav-link"}
+            `}
+            >
+              Home
+            </li>
           </Link>
           <Link href="/about">
-            <li className="nav-link">About Us</li>
+            <li
+              className={` cursor-pointer
+              ${pathname === "/about" ? "active-nav-link" : "nav-link"}
+            `}
+            >
+              About Us
+            </li>
           </Link>
           <Link href="/projects">
-            <li className="nav-link">Projects</li>
+            <li
+              className={` cursor-pointer
+              ${pathname === "projects" ? "active-nav-link" : "nav-link"}
+            `}
+            >
+              Projects
+            </li>
           </Link>
           <Link href="/news-articles">
-            <li className="nav-link">News Articles</li>
+            <li
+              className={` cursor-pointer
+              ${pathname === "/news-articles" ? "active-nav-link" : "nav-link"}
+            `}
+            >
+              News Articles
+            </li>
           </Link>
           <li className="nav-link">Blogs</li>
         </ul>
@@ -74,9 +99,16 @@ export default function Navbar(props) {
         {/* Contact */}
         <div className="hidden lg:block lg:pr-5">
           <button
-            className="text-xl text-white border-secondary border-[1px] rounded-xl px-3 py-2
-          hover:bg-secondary hover:text-white transition duration-300 ease-in-out
-          "
+            className={`text-xl  border-secondary border-[1px] rounded-xl px-3 py-2
+          duration-300 ease-in-out
+
+          ${
+            pathname === "/contact"
+              ? "bg-secondary text-black font-semibold"
+              : "hover:bg-secondary hover:text-white transition text-white"
+          }
+
+          `}
           >
             <Link href="/contact">Contact Us</Link>
           </button>
@@ -105,50 +137,113 @@ export default function Navbar(props) {
         } lg:hidden`}
       >
         <div className="flex items-center justify-between py-3">
-          <div className="flex items-center gap-1">
-            <div>
-              <Image
-                src="/logo.png"
-                height={50}
-                width={50}
-                alt="Loud & Clear"
-              />
+          <Link href="/">
+            <div className="flex items-center gap-1">
+              <div>
+                <Image
+                  src="/logo.png"
+                  height={50}
+                  width={50}
+                  alt="Loud & Clear"
+                />
+              </div>
+              <div>
+                <h1 className={`${brandBe.className} text-black text-[42px]`}>
+                  Loud & Clear
+                </h1>
+              </div>
             </div>
-            <div>
-              <h1 className={`${lora.className} text-black text-2xl`}>
-                Loud & Clear
-              </h1>
-            </div>
-          </div>
+          </Link>
 
           {/* Cross icon div */}
-          <div>
+          <div className="pr-4">
             <RxCross2
-              className="text-black text-4xl cursor-pointer pr-2"
+              className="text-black text-3xl cursor-pointer"
               onClick={() => setIsHamburgerOpen(false)}
             />
           </div>
         </div>
 
         {/* Nav Items */}
-        <div className="mt-2 px-4">
-          <ul className="text-3xl space-y-3 font-medium">
-            <Link href="/">
-              <li>Home</li>
-            </Link>
-            <Link href="/about">
-              <li>About Us</li>
-            </Link>
-            <Link href="/projects">
-              <li>Projects</li>
-            </Link>
-            <Link href="/news-articles">
-              <li>News Articles</li>
-            </Link>
-            <li>Blogs</li>
-            <Link href="/contact">
-              <li>Contact Us</li>
-            </Link>
+        <div className="mt-5 px-12">
+          <ul className="text-3xl space-y-6 font-medium">
+            <div>
+              <Link href="/">
+                <li
+                  className={`
+                cursor-pointer
+                ${pathname === "/" ? "border-b-2 w-fit border-black" : ""}
+                `}
+                >
+                  Home
+                </li>
+              </Link>
+            </div>
+            <div>
+              <Link href="/about">
+                <li
+                  className={`
+                cursor-pointer
+                ${pathname === "/about" ? "border-b-2 w-fit border-black" : ""}
+                `}
+                >
+                  About Us
+                </li>
+              </Link>
+            </div>
+
+            <div>
+              <Link href="/projects">
+                <li
+                  className={`
+                cursor-pointer
+                ${
+                  pathname === "/projects"
+                    ? "border-b-2 w-fit border-black"
+                    : ""
+                }
+                `}
+                >
+                  Projects
+                </li>
+              </Link>
+            </div>
+            <div>
+              <Link href="/news-articles">
+                <li
+                  className={`
+                cursor-pointer
+                ${
+                  pathname === "/news-articles"
+                    ? "border-b-2 w-fit border-black"
+                    : ""
+                }
+                `}
+                >
+                  News Articles
+                </li>
+              </Link>
+            </div>
+            <div>
+              <Link href="/blogs">
+                <li>Blogs</li>
+              </Link>
+            </div>
+
+            <div>
+              <Link href="/contact">
+                <li
+                  className={`
+                cursor-pointer
+                ${
+                  pathname === "/contact" ? "border-b-2 w-fit border-black" : ""
+                }
+                `}
+                >
+                  Contact Us
+                </li>
+              </Link>
+            </div>
           </ul>
         </div>
       </div>
--- src/components/SubFooter.jsx
+++ src/components/SubFooter.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 import { FaInstagram } from "react-icons/fa6"
 import { FaLinkedinIn } from "react-icons/fa"
