ebbea87606ea9e3807f1df996e5f8e05200039da
Preet Sojitra <preet.dev373@gmail.com>
2024-07-11 12:26:46 +0530

    add analytics, sitemap and speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Roboto } from "next/font/google"
 import "./globals.css"
 
@@ -11,7 +13,7 @@ export const metadata = {
   description:
     "Premier AV solutions provider delivering crystal-clear sound & stunning visuals for tailored entertainment experiences. Expert consultations for seamless integration into any space. From home theaters to corporate environments, experience audio-video excellence with Loud & Clear.",
   keywords:
-    "audio, video, AV, solutions, home theater, corporate, Loud & Clear",
+    "audio, video, AV, solutions, home theater, corporate, Loud & Clear, Ahmedabad, Gujarat, India, sound, visual, entertainment, experience, excellence, expert, consultation, integration, space, crystal-clear, stunning, tailored, premier, provider, AV solutions, AV solutions provider, AV solutions provider in Ahmedabad, AV solutions provider in Gujarat, AV solutions provider in, Loud and Clear, loudandclear, loud and clear ahmedabad",
   image: "https://www.loudandclear.in/logo.png",
   author: "Loud and Clear",
   robots: "index, follow",
@@ -30,7 +32,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} bg-primary`}>{children}</body>
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <meta name="robots" content={metadata.robots} />
+        <meta name="language" content={metadata.language} />
+        <meta name="generator" content={metadata.generator} />
+        <meta property="og:title" content={metadata.ogTitle} />
+        <meta property="og:type" content={metadata.ogType} />
+        <meta property="og:url" content={metadata.ogUrl} />
+        <meta property="og:description" content={metadata.ogDescription} />
+        <meta property="og:image" content={metadata.ogImage} />
+      </head>
+      <body className={`${roboto.className} bg-primary`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
